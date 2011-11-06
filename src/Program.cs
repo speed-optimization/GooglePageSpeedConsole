4b41d08b362d7bed5408dc36bf3de88f69e38629
Martin Kraft <martinkraft@gmail.com>
2011-11-06 17:12:47 -0500

    Added documentation to the class and the compiler switch to generate the associated XML file.

--- src/Program.cs
+++ src/Program.cs
@@ -7,6 +7,12 @@ namespace GooglePageSpeedConsole
 {
 	class Program
 	{
+		/** 
+		<summary>Main entry point to the console app. Uses Google Page Speed's API to Console.Write the Page Speed score of a given URL.</summary>
+		<param name="args[0]">String. Required. The API key to Google Page Speed.</param>
+		<param name="args[1]">String. Required. The URL of the site to test.</param>
+		<example><c>GooglePageSpeedConsole.exe 08234klfuiop093248jlKa http://www.hotmail.com</c></example>
+		*/
 		static void Main(string[] args)
 		{
 			string json = GetJsonFromGoogle(args[0], args[1]);
